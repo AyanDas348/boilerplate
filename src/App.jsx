e7926a6deaaa4005cdd3870a7020ca90ddc73d39
Ayan Das <dasayan348@gmail.com>
2023-06-19 09:28:33 +0530

    Boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { Navbar, Hero, Section1, Section2, Contact } from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar/>
+          <Hero/>
+          <Section1/>
+          <Section2/>
+          <Contact/>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+import Hero from "./Hero"
+import Section1 from "./Section1"
+import Section2 from "./Section2"
+import Contact from "./Contact"
+
+export {
+    Navbar,
+    Hero,
+    Section1,
+    Section2,
+    Contact
+}
